91ead2ab1d61889ee021e5d138e4dde0b1554942
Brian Webster <bwebster@iastate.edu>
2019-09-09 14:21:38 -0500

    Try to handle pulling in an article that is pulling in an article

--- src/Plugin/Field/FieldFormatter/NewsEmbedFieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/NewsEmbedFieldDefaultFormatter.php
@@ -97,8 +97,10 @@ class NewsEmbedFieldDefaultFormatter extends FormatterBase {
       $dom = $this->load_html($html);
       if($dom) {
         $articles = $dom->getElementsByTagName('article');
-        if (count($articles)) {
+        if (count($articles) == 2) {
           $results = $articles[0]->ownerDocument->saveXML($articles[0]);
+        } elseif (count($articles) == 3) {
+          $results = $articles[0]->ownerDocument->saveXML($articles[1]);
         }
       }
     }
